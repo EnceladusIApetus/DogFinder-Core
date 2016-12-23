9f6e2dc27ac3c05eb1c711bfbd63e6688d1a2687
Pakin Boonchoopirach <moobinkung@windowslive.com>
2016-12-23 23:05:08 +0700

    - improved nearest_neighbors module

--- modules/nearest_neighbors.py
+++ modules/nearest_neighbors.py
@@ -1,22 +1,24 @@
 from sklearn.neighbors import NearestNeighbors
 
-nn = None
+model = None
 neighbor_num = None
 radius = None
 
+
 def set(neighbor_num, radius):
-    global nn
+    global model
     globals()['neighbor_num'] = neighbor_num
     globals()['radius'] = radius
-    nn = NearestNeighbors(neighbor_num, radius)
+    model = NearestNeighbors(neighbor_num, radius)
+
 
 def fit(data):
-    global nn, neighbor_num, radius
+    global model, neighbor_num, radius
     if len(data) < neighbor_num:
-        nn = NearestNeighbors(len(data), radius) # if number of neighbor is less than defined
-    nn.fit(data)
+        model = NearestNeighbors(len(data), radius) # if number of neighbor is less than defined
+    model.fit(data)
+
 
 def neighbors(data):
-    global nn
-    hello = nn
-    return hello.kneighbors(data, return_distance=False)
+    global model
+    return model.kneighbors(data, return_distance=False)[0]
